3663b90facd667ee21e28b775c398dd0ee64eac7
Peter Erian <peterchristof.erian@gmail.com>
2021-08-31 17:00:56 +0100

    Solved Lab

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -19,7 +20,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  //Promises are Javascript Objects that might have or not a value in the near future
+  res.render("index");
+  });
+  //.catch(error=> {
+//  console(error);
+//  });
+
+
+//Traditional Way to consume promises
+//app.get("/beers", (req, res) => {
+ // punkAPI.getBeers().then(beers => {
+ //   console.log(beers);
+ //   res.render('beers', {beers});
+//});
+//});
+
+//More modern way to consume promises
+app.get("/beers", async (req, res) => {
+  const beers = await punkAPI.getBeers();
+  res.render('beers', {beers});
 });
 
+app.get("/random-beers", async (req, res) => {
+ punkAPI.getRandom().then(response => {
+ console.log(response);
+ res.render('random-beers', {response});
+})
+.catch(error => console.log(error));
+});
+
+  
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
